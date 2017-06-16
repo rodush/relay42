4b78f489cb50d0e15607fbf7c330b6be4f221aba
Roman Dushko <rodush@gmail.com>
2017-06-16 17:30:17 +0200

    Introduced App as a higher order component, decorated with WebSocket

--- src/App.js
+++ src/App.js
@@ -15,49 +15,49 @@ const APP_CONFIG = {
 };
 
 let gen = dataGenerator();
-let ws;
+let ws = new WebSocket(APP_CONFIG.wsUrl, "v1");
 
-class App extends Component {
-    componentWillMount() {
-        // Open WS connection
-        ws = new WebSocket(APP_CONFIG.wsUrl, "v1");
+// Higher order component.
+// Composed to inject WebSocket instance into Application.
+function connectedApp(WrappedComponent, ws) {
+    return class extends Component {
+        componentWillMount() {
+            ws.onerror = (err) => console.log(err);
+            ws.onopen = () => {
+                console.log("Hello wizard!");
+            };
+            ws.onmessage = (evt = {}) => {
+                if (!evt.data) {
+                    return;
+                }
 
-        ws.onerror = (err) => console.log(err);
-        ws.onopen = () => {
-            console.log("Hello wizard!");
-        };
-        ws.onmessage = (evt = {}) => {
-            if (!evt.data) {
-                return;
+                let msg = JSON.parse(evt.data);
+                store.dispatch(actionMakers.dataReceived(msg));
             }
-
-            let msg = JSON.parse(evt.data);
-            store.dispatch(actionMakers.dataReceived(msg));
         }
-    }
 
-    componentWillUnmount() {
-        ws.close();
-    }
-
-    componentDidMount() {
-        let chart = this.refs.chart.getChart();
-        let nextVal = gen.next().value;
-        while (nextVal) {
-            chart.series[0].addPoint(buildPoint(nextVal, 'totalCallsAdded'), true);
-            chart.series[1].addPoint(buildPoint(nextVal, 'totalCallsRemoved'), true);
-            chart.series[2].addPoint(buildPoint(nextVal, 'segmentSize'), true);
-            nextVal = gen.next().value;
+        componentWillUnmount() {
+            ws.close();
         }
-    }
 
-    render() {
-        return (
-            <ReactHighcharts config={HC_CONFIG} ref="chart"></ReactHighcharts>
-        );
-    }
+        componentDidMount() {
+            let chart = this.refs.chart.getChart();
+            let nextVal = gen.next().value;
+            while (nextVal) {
+                chart.series[0].addPoint(buildPoint(nextVal, 'totalCallsAdded'), true);
+                chart.series[1].addPoint(buildPoint(nextVal, 'totalCallsRemoved'), true);
+                chart.series[2].addPoint(buildPoint(nextVal, 'segmentSize'), true);
+                nextVal = gen.next().value;
+            }
+        }
+
+        render() {
+            return <WrappedComponent config={HC_CONFIG} ref="chart" {...this.props} />;
+        }
+    };
 }
 
+const App = connectedApp(ReactHighcharts, ws);
 
 //-----------
 // PRIVATE
